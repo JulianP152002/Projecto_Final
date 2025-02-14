5052fbeff8e3d04175c9d6076ea28d2948e161b7
JulianP152002 <parracardenas17@gmail.com>
2025-02-13 21:57:28 -0500

    correction name variable

--- back/src/config.js
+++ back/src/config.js
@@ -1,4 +1,4 @@
 const MONGODB_URI = process.env.MONGODBURI || 'mongodb://localhost:27017/dbtest' 
 const PORT =  process.env.PORT || 3000
 
-export { MONGODBURI, PORT }
+export { MONGODB_URI, PORT }
