b82edaff0eaa8ce691404e6038bdd37fc8b73105
JulianP152002 <parracardenas17@gmail.com>
2025-02-13 21:54:50 -0500

    change URIDB

--- back/src/config.js
+++ back/src/config.js
@@ -1,4 +1,4 @@
-const MONGODBURI = process.env.MONGODBURI || 'mongodb://localhost:27017/dbtest' 
+const MONGODB_URI = process.env.MONGODBURI || 'mongodb://localhost:27017/dbtest' 
 const PORT =  process.env.PORT || 3000
 
 export { MONGODBURI, PORT }
--- back/src/db.js
+++ back/src/db.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
-import {MONGODBURI} from "./config.js";
+import {MONGODB_URI} from "./config.js";
 mongoose.connect('mongodb://localhost:27017/dbtest')
   .then(db => console.log('data base running'))
