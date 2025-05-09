563617ec2a9dcbfd204a5da096cab84a72a1603c
JulianP152002 <parracardenas17@gmail.com>
2025-05-08 19:15:31 -0500

    protected all rutes

--- back/src/index.js
+++ back/src/index.js
@@ -5,17 +5,37 @@ import { SaveProducts } from "./controllers/controllers.js";
 import { DeleteProducts } from "./controllers/controllers.js";
 import { UpdateProducts } from "./controllers/controllers.js";
 import { upload } from "./config/multer.js";
+import rateLimit from "express-rate-limit";
 
-app.get("/products", getProducts);
+const getLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // Hasta 300 solicitudes por IP
+  message: {
+    status: false,
+    error: "Demasiadas solicitudes GET. Intenta más tarde.",
+  },
+});
+
+const changeLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20, // Solo 20 solicitudes en 15 minutos
+  message: {
+    status: false,
+    error: "Demasiadas acciones de escritura. Intenta más tarde.",
+  },
+});
+app.get("/products", getLimiter, getProducts);
 
 app.post(
   "/products",
+  changeLimiter,
   upload.fields([{ name: "image", maxCount: 1 }]),
   SaveProducts
 );
-app.delete("/products/:id", DeleteProducts);
+app.delete("/products/:id", changeLimiter, DeleteProducts);
 app.put(
   "/products/:id",
+  changeLimiter,
   upload.fields([{ name: "image", maxCount: 1 }]),
   UpdateProducts
 );
